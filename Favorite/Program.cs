26a5ba96ac1627acd238f86f23e561ef95696109
Andrew <lanadelku@gmail.com>
2022-10-07 17:16:08 +0300

    Favorite Name

---
+++ Favorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "андрей")
+{
+    Console.WriteLine("Ура, ты тут!");
+}
+else 
+{
+    Console.Write("Ну, привет, ");
+    Console.Write(username.ToUpper());  
+}
